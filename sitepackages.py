a9b69780f63ada43d192ed801703cdc72bffc1cc
Thomas Upton <tom@shipwell.com>
2020-12-09 23:20:56 -0600

    (Re-)add sitepackages.py

---
+++ sitepackages.py
@@ -0,0 +1,27 @@
+from __future__ import print_function
+
+import sys
+import site
+
+def is_virtual_env():
+    return hasattr(sys, 'real_prefix')
+
+def virtual_env_site_packages():
+    site_packages = [p for p in sys.path if p.endswith('site-packages')]
+    if len(site_packages) > 0:
+        return site_packages[0]
+
+def site_packages():
+    site_packages = site.getsitepackages()
+    if len(site_packages) > 0:
+        return site_packages[0]
+
+def get_site_packages():
+    if is_virtual_env():
+        return virtual_env_site_packages()
+    else:
+        return site_packages()
+
+if __name__ == '__main__':
+    print('{}'.format(get_site_packages()))
+
